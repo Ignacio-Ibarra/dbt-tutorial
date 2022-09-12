fa8212e89474cea43507874e3dd17c67afda5d5b
Ignacio-ibarra <ignacioibarra.data.science@gmail.com>
2022-09-12 10:42:34 -0300

    changes

--- empleo_proj/models/join_tablas/join_tablas.sql
+++ empleo_proj/models/join_tablas/join_tablas.sql
@@ -1,42 +1,16 @@
-{{config(materialized='view') }}
+{{config(materialized='table') }}
 
-with puestos as (
 
-    select
-        fecha,
-        codigo_departamento_indec,
-        clae2,
-        puestos
+select
+p.fecha as Fecha,
+p.codigo_departamento_indec as CodDepto,
+p.clae2 as Clae,
+p.puestos as Puestos,
+s.w_mean as Salarios
+from prueba_1.puestos as p inner join prueba_1.salarios as s
+on p.fecha = s.fecha 
+and p.codigo_departamento_indec = s.codigo_departamento_indec
+and p.clae2 = s.clae2
+limit 10
 
-    from prueba_1.puestos
 
-),
-
-with salarios as (
-
-    select
-        fecha,
-        codigo_departamento_indec,
-        clae2,
-        w_mean
-
-    from prueba_1.salarios
-
-), 
-
-with final as (
-
-    select
-        p.fecha as Fecha,
-        p.codigo_departamento_indec as CodDepto,
-        p.clae2 as Clae,
-        p.puestos as Puestos,
-        s.w_mean as Salarios
-    from puestos as p inner join salarios as s
-    on p.fecha = s.fecha 
-    and p.codigo_departamento_indec = s.codigo_departamento_indec
-    and p.clae2 = s.clae2
-
-),
-
-select * from puestos
